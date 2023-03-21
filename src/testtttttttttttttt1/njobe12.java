64f23566878a2c758ef215cbe490ef494c6b817e
benjamins997 <sprecobenjamin02@gmail.com>
2023-03-21 23:31:56 +0100

    Print this message to GitHub.

--- src/testtttttttttttttt1/njobe12.java
+++ src/testtttttttttttttt1/njobe12.java
@@ -41,7 +41,7 @@ public class njobe12 {
 		driver.findElement(By.id("continue")).click();
 		Thread.sleep(2000);
 		driver.quit();
-		System.out.println("There was a undetectable problem\r\n" + "We couldnt find an account with that email address");
+		System.out.println("Print this message on GitHub." + "We couldnt find an account with that email address");
 		
 		
 	}
